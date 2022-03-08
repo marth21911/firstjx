f68de45e798e4784a7c7feff0f54fd94229210fb
Brian Marth <marthbrian@gmail.com>
2022-03-08 17:52:08 -0500

    firstjsx

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Hello Dojo!</h1>
+          <h2>Things I need to do:</h2>
+          <ul>
+            <li>Learn React</li>
+            <li>Climb Mt Trogdor</li>
+            <li>Run a manatee farm</li>
+            <li>Feed the neighbors dog too often thereby making it obese and less barky.</li>
+          </ul>
       </header>
     </div>
   );
